b7be8292bddbc0834af478996df6832910c1aab3
derickdr <akashi.hiro@gmail.com>
2024-12-19 13:08:19 -0800

    create initial files

---
+++ src/commands.js
@@ -0,0 +1,14 @@
+export const IDIOT_COMMAND = {
+    name: 'idiot',
+    description: 'add \"idiot\" role to a member.',
+  };
+  
+export const INVITE_COMMAND = {
+    name: 'not idiot',
+    description: 'remove \"idiot\" role from a member.',
+};
+
+export const STARE_COMMAND = {
+    name: 'stare',
+    description: 'return a randomly selected image from a pre-selected array.',
+};
---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*
+
+function idiot(invokingMember, targetMember) {
+    let permission = invokingMember.member.guild.roles.get('admin')
+    if (!permission) {
+        return 'Not an idiot smasher, idiot.'
+    } else {
+        targetMember.addRole('idiot')
+        return 'idiot.' -> in #idiots-only
+    }
+}
+
+function unidiot(invokingMember, targetMember) { 
+}
+
+function stare(member, message) { 
+}
+
+*/
